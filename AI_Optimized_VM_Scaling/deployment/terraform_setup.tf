5348cbff0a79f54c93eebbac57610156246ed641
M23AID065 <m23aid065@iitj.ac.in>
2025-03-29 14:31:46 +0530

    Update terraform_setup.tf

--- AI_Optimized_VM_Scaling/deployment/terraform_setup.tf
+++ AI_Optimized_VM_Scaling/deployment/terraform_setup.tf
@@ -1,1 +1,10 @@
-# Terraform script for infrastructure setup
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_autoscaling_group" "asg" {
+  desired_capacity     = 3
+  max_size            = 10
+  min_size            = 2
+  vpc_zone_identifier = ["subnet-abc123"]
+}
